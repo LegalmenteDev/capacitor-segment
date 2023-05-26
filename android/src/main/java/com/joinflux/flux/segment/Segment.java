06a387395389c7ac6003c84b749b988d04c562cc
Luis Rhenals <luis.rhenals@bitsamericas.com>
2023-05-26 10:05:57 -0500

    Fix null identify traits

--- android/src/main/java/com/joinflux/flux/segment/Segment.java
+++ android/src/main/java/com/joinflux/flux/segment/Segment.java
@@ -15,71 +15,82 @@ import com.segment.analytics.Analytics.Builder;
 import com.segment.analytics.Options;
 import com.segment.analytics.Properties;
 import com.segment.analytics.Traits;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+
 import org.json.JSONException;
 
 public class Segment {
 
-    private static final String PLUGIN_TAG = "Segment";
-    public Analytics analytics;
+  private static final String PLUGIN_TAG = "Segment";
+  public Analytics analytics;
 
-    public void identify(String userId, @Nullable JSObject traits) {
-        this.analytics.identify(userId, makeTraitsFromMap(makeMapFromJSON(traits)), null);
-    }
+  public void identify(String userId, JSObject traits) {
+    this.analytics.identify(userId, makeTraitsFromMap(makeMapFromJSON(traits)), null);
+  }
 
-    public void track(String eventName, JSObject properties, @Nullable JSObject options) {
-        this.analytics.track(eventName, makePropertiesFromMap(makeMapFromJSON(properties)));
-    }
+  public void identify(String userId) {
+    this.analytics.identify(userId);
+  }
 
-    public void page(String pathname) {
-        this.analytics.screen(pathname);
-    }
 
-    public void reset() {
-        this.analytics.reset();
+  public void track(String eventName, JSObject properties, @Nullable JSObject options) {
+    if (options != null) {
+      this.analytics.track(eventName, makePropertiesFromMap(makeMapFromJSON(properties)));
+    } else {
+      this.analytics.track(eventName);
     }
+  }
 
-    private Map<String, Object> makeMapFromJSON(JSObject obj) {
-        Iterator<String> keys = obj.keys();
-        Map<String, Object> map = new HashMap<String, Object>();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            try {
-                Object value = obj.get(key);
-                map.put(key, value);
-            } catch (JSONException e) {
-                Log.d(PLUGIN_TAG, "could not get value for key " + key);
-            }
-        }
-        return map;
-    }
+  public void page(String pathname) {
+    this.analytics.screen(pathname);
+  }
 
-    private Traits makeTraitsFromMap(Map<String, Object> map) {
-        Traits traits = new Traits();
-        traits.putAll(map);
-        return traits;
-    }
+  public void reset() {
+    this.analytics.reset();
+  }
 
-    private Properties makePropertiesFromMap(Map<String, Object> map) {
-        Properties properties = new Properties();
-        properties.putAll(map);
-        return properties;
+  private Map<String, Object> makeMapFromJSON(JSObject obj) {
+    Iterator<String> keys = obj.keys();
+    Map<String, Object> map = new HashMap<String, Object>();
+    while (keys.hasNext()) {
+      String key = keys.next();
+      try {
+        Object value = obj.get(key);
+        map.put(key, value);
+      } catch (JSONException e) {
+        Log.d(PLUGIN_TAG, "could not get value for key " + key);
+      }
     }
+    return map;
+  }
 
-    private Options makeOptionsFromJSON(JSObject obj) {
-        Options options = new Options();
-        Iterator<String> keys = obj.keys();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            try {
-                boolean enabled = obj.getBool(key);
-                options.setIntegration(key, enabled);
-            } catch (Exception e) {
-                Log.d(PLUGIN_TAG, "could not get boolean for key " + key);
-            }
-        }
-        return options;
+  private Traits makeTraitsFromMap(Map<String, Object> map) {
+    Traits traits = new Traits();
+    traits.putAll(map);
+    return traits;
+  }
+
+  private Properties makePropertiesFromMap(Map<String, Object> map) {
+    Properties properties = new Properties();
+    properties.putAll(map);
+    return properties;
+  }
+
+  private Options makeOptionsFromJSON(JSObject obj) {
+    Options options = new Options();
+    Iterator<String> keys = obj.keys();
+    while (keys.hasNext()) {
+      String key = keys.next();
+      try {
+        boolean enabled = obj.getBool(key);
+        options.setIntegration(key, enabled);
+      } catch (Exception e) {
+        Log.d(PLUGIN_TAG, "could not get boolean for key " + key);
+      }
     }
+    return options;
+  }
 }
--- android/src/main/java/com/joinflux/flux/segment/SegmentPlugin.java
+++ android/src/main/java/com/joinflux/flux/segment/SegmentPlugin.java
@@ -3,6 +3,7 @@ package com.joinflux.flux.segment;
 import android.content.Context;
 import android.net.Uri;
 import android.util.Log;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -14,124 +15,129 @@ import com.segment.analytics.ConnectionFactory;
 import com.segment.analytics.Options;
 import com.segment.analytics.Properties;
 import com.segment.analytics.Traits;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 
 @CapacitorPlugin(name = "Segment")
 public class SegmentPlugin extends Plugin {
 
-    private static boolean initialized = false;
-    private static Segment implementation = new Segment();
+  private static boolean initialized = false;
+  private static Segment implementation = new Segment();
 
-    @PluginMethod
-    public void initialize(PluginCall call) {
-        synchronized (implementation) {
-            // No-op
-            if (initialized == true) {
-                call.resolve();
-                return;
-            }
-
-            String key = call.getString("key");
-            if (key == null) {
-                call.reject("Write key is required to initialize plugin");
-                return;
-            }
-
-            Context context = this.getContext();
-
-            Builder builder;
-            String proxyHost = call.getString("proxyHost");
-
-            if (proxyHost == null) {
-                builder = new Analytics.Builder(context, key);
-            } else {
-                builder =
-                    new Analytics.Builder(context, key)
-                        .connectionFactory(
-                            new ConnectionFactory() {
-                                @Override
-                                protected HttpURLConnection openConnection(String url) throws IOException {
-                                    String path = Uri.parse(url).getPath();
-                                    return super.openConnection(proxyHost + path);
-                                }
-                            }
-                        );
-            }
-
-            Boolean trackLifecycle = call.getBoolean("trackLifecycle", false);
-            if (trackLifecycle) {
-                builder.trackApplicationLifecycleEvents().experimentalUseNewLifecycleMethods(false);
-            }
-
-            Boolean recordScreenViews = call.getBoolean("recordScreenViews", false);
-            if (recordScreenViews) {
-                builder.recordScreenViews();
-            }
-            initialized = true;
-            implementation.analytics = builder.build();
-            call.resolve();
-        }
-    }
-
-    @PluginMethod
-    public void identify(PluginCall call) {
-        if (!initialized) {
-            call.reject("Segment is not initialized");
-            return;
-        }
-        String userId = call.getString("userId");
-        if (userId == null) {
-            call.reject("User ID is required for 'identify' but not supplied");
-            return;
-        }
-        JSObject traits = call.getObject("traits");
-
-        implementation.identify(userId, traits);
+  @PluginMethod
+  public void initialize(PluginCall call) {
+    synchronized (implementation) {
+      // No-op
+      if (initialized == true) {
         call.resolve();
+        return;
+      }
+
+      String key = call.getString("key");
+      if (key == null) {
+        call.reject("Write key is required to initialize plugin");
+        return;
+      }
+
+      Context context = this.getContext();
+
+      Builder builder;
+      String proxyHost = call.getString("proxyHost");
+
+      if (proxyHost == null) {
+        builder = new Analytics.Builder(context, key);
+      } else {
+        builder =
+          new Analytics.Builder(context, key)
+            .connectionFactory(
+              new ConnectionFactory() {
+                @Override
+                protected HttpURLConnection openConnection(String url) throws IOException {
+                  String path = Uri.parse(url).getPath();
+                  return super.openConnection(proxyHost + path);
+                }
+              }
+            );
+      }
+
+      Boolean trackLifecycle = call.getBoolean("trackLifecycle", false);
+      if (trackLifecycle) {
+        builder.trackApplicationLifecycleEvents().experimentalUseNewLifecycleMethods(false);
+      }
+
+      Boolean recordScreenViews = call.getBoolean("recordScreenViews", false);
+      if (recordScreenViews) {
+        builder.recordScreenViews();
+      }
+      initialized = true;
+      implementation.analytics = builder.build();
+      call.resolve();
+    }
+  }
+
+  @PluginMethod
+  public void identify(PluginCall call) {
+    if (!initialized) {
+      call.reject("Segment is not initialized");
+      return;
+    }
+    String userId = call.getString("userId");
+    if (userId == null) {
+      call.reject("User ID is required for 'identify' but not supplied");
+      return;
+    }
+    JSObject traits = call.getObject("traits");
+    if (traits != null) {
+      implementation.identify(userId, traits);
+    } else {
+      implementation.identify(userId);
     }
 
-    @PluginMethod
-    public void track(PluginCall call) {
-        if (!initialized) {
-            call.reject("Segment is not initialized");
-            return;
-        }
-        String eventName = call.getString("eventName");
-        if (eventName == null) {
-            call.reject("Event name is not supplied");
-            return;
-        }
-        JSObject properties = call.getObject("properties");
-        JSObject options = call.getObject("options");
+    call.resolve();
+  }
 
-        implementation.track(eventName, properties, options);
-        call.resolve();
+  @PluginMethod
+  public void track(PluginCall call) {
+    if (!initialized) {
+      call.reject("Segment is not initialized");
+      return;
+    }
+    String eventName = call.getString("eventName");
+    if (eventName == null) {
+      call.reject("Event name is not supplied");
+      return;
+    }
+    JSObject properties = call.getObject("properties");
+    JSObject options = call.getObject("options");
+
+    implementation.track(eventName, properties, options);
+    call.resolve();
+  }
+
+  @PluginMethod
+  public void page(PluginCall call) {
+    if (!initialized) {
+      call.reject("Segment is not initialized");
+      return;
+    }
+    String pathname = call.getString("pathname");
+    if (pathname == null) {
+      call.reject("Pathname was not supplied");
+      return;
     }
 
-    @PluginMethod
-    public void page(PluginCall call) {
-        if (!initialized) {
-            call.reject("Segment is not initialized");
-            return;
-        }
-        String pathname = call.getString("pathname");
-        if (pathname == null) {
-            call.reject("Pathname was not supplied");
-            return;
-        }
+    implementation.page(pathname);
+    call.resolve();
+  }
 
-        implementation.page(pathname);
-        call.resolve();
+  @PluginMethod
+  public void reset(PluginCall call) {
+    if (!initialized) {
+      call.reject("Segment is not initialized");
+      return;
     }
-
-    @PluginMethod
-    public void reset(PluginCall call) {
-        if (!initialized) {
-            call.reject("Segment is not initialized");
-            return;
-        }
-        implementation.reset();
-        call.resolve();
-    }
+    implementation.reset();
+    call.resolve();
+  }
 }
